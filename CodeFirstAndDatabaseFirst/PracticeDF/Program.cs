568ee22a1d0cfb76c6081aebc87cc271eae14a4b
FilipMilosevski <filipdh94@gmail.com>
2023-07-19 20:33:16 +0200

    19 07 sql inners

---
+++ CodeFirstAndDatabaseFirst/PracticeDF/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Filip;
+using Microsoft.EntityFrameworkCore;
+
+static void Filter1()
+{
+    using (SimpleDbgegoraphyContext db = new SimpleDbgegoraphyContext())
+    {
+        DbSet<Country> countries = db.Countries;
+        DbSet<Language> languages = db.Languages;
+        foreach (Country p in countries)
+        {
+            Console.WriteLine(p.Name);
+           
+        }
+        foreach (Language l in languages)
+        {
+            Console.WriteLine(l.Language1) ;
+        }
+    }
+}
+Filter1();
