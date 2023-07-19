b23fe2a0e222bc863d29d1ac2f0af4dc456deddd
FilipMilosevski <filipdh94@gmail.com>
2023-07-19 20:55:17 +0200

    1907 inner joins sql

--- CodeFirstAndDatabaseFirst/PracticeDF/Program.cs
+++ CodeFirstAndDatabaseFirst/PracticeDF/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 using Filip;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
-static void Filter1()
+static void Filter3()
 {
     using (SimpleDbgegoraphyContext db = new SimpleDbgegoraphyContext())
     {
@@ -10,7 +11,7 @@ static void Filter1()
         DbSet<Language> languages = db.Languages;
         foreach (Country p in countries)
         {
-            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Name,p.Region);
            
         }
         foreach (Language l in languages)
@@ -19,4 +20,58 @@ static void Filter1()
         }
     }
 }
-Filter1();
+Filter3();
+
+ static void ExecuteQueryAndPrintResults(string connectionString)
+{
+    string sqlQuery = @"
+            SELECT
+                Regions.continentID,
+                Continents.name,
+                Countries.name,
+                Countries.countryCode2,
+                CountryLanguages.official,
+                Languages.languageID,
+                CountryStats.year,
+                CountryStats.population,
+                CountryStats.gdp
+            FROM Continents
+            INNER JOIN Regions ON Continents.continentID = Regions.continentID
+            INNER JOIN Countries ON Regions.regionID = Countries.regionID
+            INNER JOIN CountryLanguages ON Countries.countryID = CountryLanguages.countryID
+            INNER JOIN Languages ON CountryLanguages.languageID = Languages.languageID
+            INNER JOIN CountryStats ON Countries.countryID = CountryStats.countryID";
+
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int continentID = reader.GetInt32(0);
+                string continentName = reader.GetString(1);
+                string countryName = reader.GetString(2);
+                string countryCode2 = reader.GetString(3);
+                bool isOfficial = reader.GetBoolean(4);
+                int languageID = reader.GetInt32(5);
+                int year = reader.GetInt32(6);
+                long population = reader.GetInt64(7);
+                decimal gdp = reader.GetDecimal(8);
+
+                Console.WriteLine($"Continent: {continentName}, Country: {countryName}, Population: {population}, GDP: {gdp}");
+            }
+
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
+
