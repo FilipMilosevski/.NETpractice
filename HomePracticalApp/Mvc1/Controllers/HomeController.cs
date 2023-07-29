e9f2a7332afc204dbbd8b7e2e7da009b917f576e
FilipMilosevski <filipdh94@gmail.com>
2023-07-29 12:13:52 +0200

    mvc saturday

---
+++ HomePracticalApp/Mvc1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc1.Models;
+using Mvc1.MyFolder;
+using System.Diagnostics;
+
+namespace Mvc1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly SimpleDbgegoraphyContext _context;
+
+
+        public HomeController(ILogger<HomeController> logger, SimpleDbgegoraphyContext injectedContext)
+		{
+			_logger = logger;
+			_context = injectedContext;
+		}
+		 
+		public IActionResult Index()
+		{
+			HomeIndexViewModel model = new HomeIndexViewModel();
+			VisitorCount: Random.Shared.Next(1, 100);
+			Countries: _context.Countries.ToList();
+			CountryStats: _context.CountryStats.ToList();
+
+            return View(model);
+		} 
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ HomePracticalApp/Mvc1/Models/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mvc1.MyFolder;
+
+namespace Mvc1.Models
+{
+    public record HomeIndexViewModel
+    {
+        int VisitorCount;
+       
+        public IList<Country> Countries;
+        public IList<CountryStat> CountryStats;
+
+
+    }
+}
