90910d34c067ef6e6d33026254554e661d4d52df
FilipMilosevski <filipdh94@gmail.com>
2023-08-02 23:29:49 +0200

    mvc 02082023

--- HomePracticalApp/Mvc2/Controllers/HomeController.cs
+++ HomePracticalApp/Mvc2/Controllers/HomeController.cs
@@ -21,9 +21,28 @@ namespace Mvc2.Controllers
             HomeIndexViewModel model = new HomeIndexViewModel(
                  VisitorCount: Random.Shared.Next(1, 100),
                  Categories: _context.Categories.ToList(),
-                 Products: _context.Products.ToList()
+                 Products: _context.Products.ToList(),
+                 Suppliers: _context.Suppliers.ToList(),
+                 Employees: _context.Employees.ToList(),
+                 Orders: _context.Orders.ToList(),
+                 OrderDetails: _context.OrderDetails.ToList(),
+                 Customers: _context.Customers.ToList(),
+                 Shippers: _context.Shippers.ToList()
                  );
             return View(model);
+        }
+        public IActionResult ProductDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
+            }
+            Product product = _context.Products.SingleOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound($"ProductId {id} not found.");
+            }
+            return View(product);
         }
 
         public IActionResult Privacy()
